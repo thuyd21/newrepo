7575417079d6b417c41fc091f8097ad318d1894d
thuyd <duong.le2160@icloud.com>
2024-01-15 21:03:53 +0000

    yepyep

--- week2/textadventure.py
+++ week2/textadventure.py
@@ -1,15 +1,16 @@
 # text adventure game 
 
-
-# first of all what is this startgame() 
+# first of all what is this startgame() = is a function call
+#
 
 name = input('Please enter you name:')
-print('Hello ' + name + '!!!')
+print('\nHello ' + name + '!!!')
 
 def startgame():
-    print("Welcome to the Text Adventure Game " + name + '!!')
+    print("\nWelcome to the Text Adventure Game " + name + '!!')
     print("You find yourself in a dark room. There are two doors in front of you.")
 
+# "\n" is an escape sequence representing a newline to seperate from the top line. for better formattin
     while True:
         print("\nOptions:")
         print("1. Go through the left door")
@@ -19,13 +20,15 @@ def startgame():
         choice = input("Enter your choice (1/2/3): ")
 
         if choice == "1":
-            print("You enter a magical garden. You win!")
+            print("\nYou enter a magical garden. You win!")
             break
         elif choice == "2":
-            print("You enter a dragon's lair. Game over!")
+            print("\nYou enter a dragon's lair.")
+            print("You lose. Game Over.")
             break
         elif choice == "3":
-            print("Quitting the game. Goodbye!")
+            print("\nQuitting the game...")
+            print("Goodbye!")
             break
         else:
             print("Invalid choice. Try again.")
