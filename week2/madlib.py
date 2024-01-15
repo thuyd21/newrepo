b99c3efd22e59cf852cef5a6ec8132a8852f6455
thuyd <duong.le2160@icloud.com>
2024-01-15 14:33:31 +0000

    up

---
+++ week2/madlib.py
@@ -0,0 +1,7 @@
+# mad lib word game 
+# rules:
+# one player acts as the reader
+# They ask the other players, who have no knowledge of the story, to fill in the blanks by choosing adjectives, nouns, verbs, etc. The result is a funny story
+
+name = input('What is your name?')
+print = 
